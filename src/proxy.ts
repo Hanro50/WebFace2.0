320804d514597d24857cbd25398e9a716dc71c33
Hanro Lareman <hlareman2@gmail.com>
2023-05-14 14:59:22 +0200

    Fixed crash bug

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,9 +10,6 @@ import { dataFile, mapObj, proxy, proxyServerConfig } from "./constants.js";
 import internal from "stream";
 import { TLSSocket } from "tls";
 
-const nfport = 18000;
-
-
 //Converts a given set of Proxies into an object
 export function getProxiesObj() {
     const obj: mapObj<proxy> = {};
@@ -43,6 +40,24 @@ if (existsSync(join(dataFile, "proxies.json"))) {
 
 
 if (cluster.isWorker) {
+    const errorHTML = readFileSync("./error.html").toString();
+    function sendError(client: internal.Duplex | TLSSocket, code: number, message: string) {
+        try {
+            let j: proxy[] = [];
+            proxies.forEach(e => {
+                if (!e.hide) j.push(e);
+            })
+            const content = errorHTML.replace("{code}", code.toString()).replace("{message}", message).replace("{json}", JSON.stringify(j));
+            const header = `HTTP/1.1 ${code} ERROR\nContent-Type: text/html\nKeep-Alive: timeout=5\nConnection: keep-alive\nContent-Length: ${content.length}\n\n`
+            //@ts-ignore
+            client.write(header + content);
+        } catch (e) {
+            console.error("Failed to send error message!", e)
+        } finally {
+            client.end();
+        }
+    }
+
     process.on("message", (msg) => {
         if (msg == reloadCom) reload();
         console.log(`Worker ${process.pid} reloaded proxy lists!`)
@@ -63,50 +78,26 @@ if (cluster.isWorker) {
                 })
                 const data = raw.toString() as string;
                 if (!data.split("\n")[0].includes('HTTP')) {
-                    console.log('Data of rejected header->', data)
+                    console.log('Data of rejected header->', data);
+                    sendError(client, 400, "Malformed data header!")
                     return client.end();
                 }
 
                 const hostname = data
                     .split('Host: ')[1]?.split('\r\n')[0].split(":")[0];
 
-                const result = proxies.get(hostname || "");
-                let port = result != null ? result.port : nfport;
+                const result = proxies.get(hostname);
+                if (!result) {
+                    return sendError(client, 404, "Page not found!");
+                }
+
+                let port = result != null ? result.port : 8080;
                 let host = result != null ? result.prxy : "localhost";
 
                 //   Upgrade: websocket
+
                 let server = net.createConnection({ host, port }, () => {
-                    let ended = false;
-                    function end() {
-                        if (ended) return;
-                        ended = true;
-                        if (data.startsWith("POST"))
-                            console.log("END \n%s\n_", data)
-                        client.end();
-                        server.end();
-
-                        //Freeing the internal resources. Since node's Garbage collector isn't always fast enough for this
-                        client.destroy();
-                        server.destroy();
-                    }
                     try {
-                        if (client.destroyed) { console.error("Client already dead!"); end() };
-                        // console.log('PROXY TO SERVER SET UP');
-
-                        client.on('end', () => end());
-                        server.on('end', () => end());
-
-                        client.on('error', (err) => {
-                            console.log('CLIENT TO PROXY ERROR');
-                            console.log(err);
-                            end();
-
-                        });
-                        server.on('error', (err) => {
-                            console.log('PROXY TO SERVER ERROR');
-                            console.log(err);
-                            end();
-                        });
                         server.write(data, () => {
                             client.pipe(server, { end: true });
                             server.pipe(client, { end: true });
@@ -121,6 +112,45 @@ if (cluster.isWorker) {
                     }
 
                 });
+                let ended = false;
+                function gateWayErr() {
+                    client.uncork();
+                    sendError(client, 502, "This web service is not available.")
+                }
+                function end() {
+                    if (ended) return;
+                    ended = true;
+                    if (data.startsWith("POST"))
+                        console.log("END \n%s\n_", data)
+
+
+                    client.end();
+                    server.end();
+
+                    //Freeing the internal resources. Since node's Garbage collector isn't always fast enough for this
+                    client.destroy();
+                    server.destroy();
+                }
+
+                if (client.destroyed) { console.error("Client already dead!"); end() };
+                // console.log('PROXY TO SERVER SET UP');
+                client.on('end', () => end());
+                server.on('end', () => end());
+
+                client.on('error', (err) => {
+                    console.log('CLIENT TO PROXY ERROR');
+                    console.log(err);
+                    end();
+
+                });
+                server.on('error', (err) => {
+                    console.log('PROXY TO SERVER ERROR');
+                    console.log(err);
+                    gateWayErr()
+                    end();
+                });
+
+
             });
         } catch (err) {
             console.error("Connection crash!")
@@ -158,24 +188,6 @@ if (cluster.isWorker) {
     }
 }
 else {
-    const links = new Map<string, { dist: string, type: string }>();
-    links.set("/modules/404.js", { dist: "dist/html/404.js", type: "text/javascript" });
-    links.set("/modules/util.js", { dist: "dist/html/util.js", type: "text/javascript" });
-    links.set("/css/index.css", { dist: "html/css/index.css", type: "text/css" });
-    links.set("/license.txt", { dist: "LICENSE", type: "text/plain" });
-
-    http.createServer((req, res) => {
-        const link = links.get(req.url || "") || { dist: "proxy-404.html", type: "text/html" };
-        const stat = statSync(link.dist)
-        res.writeHead(200, {
-            'Content-Type': link.type,
-            'Content-Length': stat.size
-        });
-        var readStream = createReadStream(link.dist);
-        readStream.pipe(res);
-
-        //console.log(req.url)
-    }).listen(nfport);
     //Saves the proxies given to file
     function saveProxies() {
 
